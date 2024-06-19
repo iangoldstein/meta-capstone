490c8519421c682e6d02725244bbb58bfafd9981
Ian Goldstein <ian08@goldstein.org>
2024-06-19 14:05:06 -0400

    Flushing out footer HTML

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,35 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            {/* Footer content goes here */}
+            <div className='footer-nav'>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/reservations">Reservations</a></li>
+                    <li><a href="/order-online">Order online</a></li>
+                    <li><a href="/login">Login</a></li>
+                </ul>
+            </div>
+            <div className='footer-address'>
+                <h5>Contact</h5>
+                <span>Address</span>
+                <span>Phone</span>
+                <span>Email</span>
+            </div>
+            <div className='social-links'>
+                <ul>
+                    <li>
+                        <a href="/"><img src="assets/icons/facebook.png" alt="Facebook" /></a>
+                    </li>
+                    <li>
+                        <a href="/"><img src="assets/icons/twitter.png" alt="Twitter" /></a>
+                    </li>
+                    <li>
+                        <a href="/"><img src="assets/icons/instagram.png" alt="Instagram" /></a>
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 };
--- src/components/main.js
+++ src/components/main.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Main = () => {
     return (
         <main>
-            {/* Main content goes here */}
+            <div class="section-hero">
+                <h2>Little Lemon</h2>
+            </div>
+            <div class="section-testimonial">
+                <h3>Specials</h3>
+            </div>
+            <div class="section-highlights">
+                <h4>Testimonials</h4>
+            </div>
+            <div class="section-about">
+                <h2>Little Lemon</h2>
+            </div>
         </main>
     )
 }
