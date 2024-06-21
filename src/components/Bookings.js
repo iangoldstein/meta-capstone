661deea530e8f76564b29dd683a80a0b5ad35ce0
Ian Goldstein <ian08@goldstein.org>
2024-06-21 02:12:51 -0400

    Git Hack - Complete Renaming

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,6 +1,6 @@
 // Bookings.js
 import React, { useState } from 'react';
-import BookingForm from './1BookingForm';
+import BookingForm from './BookingForm';
 
 function Bookings() {
     const [availableTimes, setAvailableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
