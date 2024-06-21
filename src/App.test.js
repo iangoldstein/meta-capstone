59068e74ea12636e0e920657b1a4c7c388e774d1
Ian Goldstein <ian08@goldstein.org>
2024-06-21 02:45:13 -0400

    Updating Testing Package

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import { initializeTimes, updateTimes } from './Bookings';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { initializeTimes, updateTimes } from './components/Bookings';
+import BookingForm from './components/BookingForm'; // Add this line
 
 test('Renders the BookingForm heading', () => {
     render(<BookingForm />);
